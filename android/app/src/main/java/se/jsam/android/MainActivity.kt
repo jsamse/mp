eecf5aee2b4c9142e96e38bd1faa40c8c66a9fd7
Jonas Samuelsson <jonas.samuelsson@tv4.se>
2021-01-31 17:01:29 +0100

    Make Android app work with static data

--- android/app/src/main/java/se/jsam/android/MainActivity.kt
+++ android/app/src/main/java/se/jsam/android/MainActivity.kt
@@ -2,10 +2,16 @@ package se.jsam.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import se.jsam.mp.DeviceInfoService
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val service = DeviceInfoService()
+        service.getDeviceInfo {
+            findViewById<TextView>(R.id.text).text = it.toString()
+        }
     }
 }
